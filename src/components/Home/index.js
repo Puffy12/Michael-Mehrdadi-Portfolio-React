597233ad55b68efef48411a20bc3bb8aa0833f4c
Michael M <113219708+Puffy12@users.noreply.github.com>
2023-12-23 20:42:20 -0800

    Fixed issues
    updated npm
    added working oontact page
    added maps locations
    added descriptions

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -38,8 +38,11 @@ const Home = () => {
             <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15} />
             <br />
             <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={22} />
+            <br />
           </h1>
+          
           <h2>Computer Science Student / Full Stack Web Developer / Software Engineer </h2>
+          
           <Link to="/contact" className="flat-button">
             CONTACT ME
           </Link>
