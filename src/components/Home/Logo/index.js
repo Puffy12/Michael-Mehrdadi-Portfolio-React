da559fc2b14f7297b8d37635ea1a864a83eb8e32
Michael M <113219708+Puffy12@users.noreply.github.com>
2024-03-21 12:01:34 -0700

    Prep For deployment 2
    -test 2

--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from 'react'
-import gsap from 'gsap'
+import { gsap } from "gsap";
 import LogoS from '../../../assets/images/big-M.png'
 import './index.scss'
 import React from 'react'
@@ -35,7 +35,6 @@ const Logo = () => {
       }
     )
   }, [])
-
   return (
     <div className="logo-container" ref={bgRef}>
       <img className="solid-logo" ref={solidLogoRef} src={LogoS} alt="JavaScript,  Developer" />
